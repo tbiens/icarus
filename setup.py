35ec189caab32af2cde1ee416ef6fd0a52883b92
Tristan <tristanbienstman@gmail.com>
2019-01-28 10:53:35 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ print (IP)
 class smtphoney:
     async def handle_RCPT(self, server, session, envelope, address, rcpt_options):
         loggingaddresses(session.peer[0], envelope.mail_from, address)
-        print("done logging address")
         envelope.rcpt_tos.append(address)
         return '250 OK'
         #straight out of documentation
