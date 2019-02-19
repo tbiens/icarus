7da67837cdaac9869a91a7ff58fd11731fde84fe
Tristan <tristanbienstman@gmail.com>
2019-02-19 14:45:26 -0500

    Update abuseipdb.py

--- abuseipdb.py
+++ abuseipdb.py
@@ -7,18 +7,22 @@ def abuseipdb(sessionpeer, mailfrom, mailto):
     config.read('icarus.config')
     abuseip = config['IPDBAPI']['AbuseIPDB']
     apikey = config['IPDBAPI']['IPDBAPI']
+    
+    headers = { 'Key': apikey,   'Accept': 'application/json', }
+    data = {'categories': '18,22'}
 
 
     if abuseip != "no":
         url = "https://api.abuseipdb.com/api/v2/report"
 
     
-
         if apikey == "PUT API KEY HERE":
             print ("This is your currently configured APIKEY in smtp.config:\n" + config['IPDBAPI']['IPDBAPI'])
         else:
-            abusepost = requests.post(url, data={'ip': sessionpeer, 'comment': 'SMTP honeypot', 'Key': apikey, 'categories': '11', 'Accept': 'application/json'})
-            print (abusepost.json())
+            abusepost = requests.post('https://api.abuseipdb.com/api/v2/report', headers=headers, data=data)
+            
+           # requests.post(url, data={'ip': sessionpeer, 'comment': 'SMTP honeypot', 'Key': apikey, 'categories': '11', 'Accept': 'application/json'})
+            print (abusepost)
             if abusepost.status_code != 200:
                 print ("HTTP code isn't 200")
 
