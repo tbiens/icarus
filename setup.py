e9c952a699cba15b55b7dd417c8dc6463b33209c
Tristan <tristanbienstman@gmail.com>
2019-02-20 10:55:59 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -64,15 +64,17 @@ class smtphoney:
         #straight out of documentation
 
     async def handle_DATA(self, server, session, envelope):
-        box1 = curses.newwin(50,100,3,0)
+        box1 = curses.newpad(50,100,3,0)
         box1.border()
-        box2 = curses.newwin(45,95,4,1)
+        box2 = curses.newpad(45,95,4,1)
         box1.refresh()
+       # box2position = 4
+        
+        
         box2.addstr(4,1,"New Email\n")
         #print ('New Email \n')
         inmemoryfile(envelope.content.decode('utf8', errors='replace')) #A function I made in memoryfile.py
-        box2.addstr(6,1,"End of Message\n")
-        box2.refresh()
+        #box2.refresh()
         #print('End of message')
         return '250 Message accepted for delivery'
     
