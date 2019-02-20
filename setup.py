7d85e619e869c1c6478f5efb0905679bea95f2d5
Tristan <tristanbienstman@gmail.com>
2019-02-20 11:02:23 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -68,7 +68,8 @@ class smtphoney:
         box1.border()
         box2 = curses.newwin(45,95,4,1)
         box1.refresh()
-        box2.addstr(4,1,"New Email\n")
+        box2.scrollok(True)
+        box2.addstr(1,1,"New Email\n")
         #print ('New Email \n')
         inmemoryfile(envelope.content.decode('utf8', errors='replace')) #A function I made in memoryfile.py
         box2.refresh()
