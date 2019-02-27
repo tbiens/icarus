2b8eda1dd95f1bed368cb4b30b2519948d9ca32b
Tristan <tristanbienstman@gmail.com>
2019-02-27 11:27:04 -0500

    Update icarussyslog.py

--- icarussyslog.py
+++ icarussyslog.py
@@ -12,7 +12,7 @@ syslogport = config['SYSLOG']['PORT']
 def syslogout(message):
     
     if syslogenable != 'no':
-        syslog = logging.handlers.SysLogHandler(address=(syslogip,syslogport),facility=LOG_SYSLOG, socktype=socket.SOCK_DGRAM)
+        syslog = logging.handlers.SysLogHandler(address=(syslogip,syslogport))
         log = logging.getLogger(__name__)
         log.addHandler(syslog)
         log.info(message)
