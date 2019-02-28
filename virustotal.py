df1fb84bc5e3907f31770719ec8a812741e48186
Tristan <tristanbienstman@gmail.com>
2019-02-28 11:12:38 -0500

    Trying out pycharm

--- virustotal.py
+++ virustotal.py
@@ -1,7 +1,7 @@
-import requests #https://developers.virustotal.com/v2.0/reference#file-scan
+import requests # https://developers.virustotal.com/v2.0/reference#file-scan
 import json
-import logging #https://docs.python.org/3.7/howto/logging.html
-import configparser #https://docs.python.org/3/library/configparser.html
+import logging # https://docs.python.org/3.7/howto/logging.html
+import configparser # https://docs.python.org/3/library/configparser.html
 
 logging.basicConfig(filename='logs/virustotal.log',level=logging.WARNING)
 
@@ -14,17 +14,20 @@ def virustotalfile(filename):
     virustotal = config['APIKEY']['Virustotal']
     URL_BASE = "https://www.virustotal.com/vtapi/v2/"
     url = URL_BASE + "file/scan"
-    #This is just straight from virustotal link above
+    # This is just straight from virustotal link above
 
     attr = {"apikey": apikey}
     files = {"file": open("downloads/" + filename, 'rb')}
     viruspost = requests.post(url, data=attr, files=files)
-    #pretty much just the API
-    if virustotal != 'no': #virustotal disabled by default.
+    # pretty much just the API
+    if virustotal != 'no': # virustotal disabled by default.
         if apikey != "PUT API KEY HERE":
             if viruspost.status_code == 200:
-                request = json.loads(viruspost.text)#The code on the virustotal page is python2, this was a pain to figure out for python3
-                print(request["verbose_msg"])#especially difficult when I didn't know there would be code differences between 2 and 3.
-                logging.warning(request["permalink"])#yaay debugging
+                request = json.loads(viruspost.text)
+                # The code on the virustotal page is python2, this was a pain to figure out for python3
+                print(request["verbose_msg"])
+                # especially difficult when I didn't know there would be code differences between 2 and 3.
+                logging.warning(request["permalink"])
+                # yaay debugging
                 print (request["permalink"]) 
 
