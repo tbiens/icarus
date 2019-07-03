1980dbc6153c1b99b088fcd74bf8259a476fdd22
Tristan <tristanbienstman@gmail.com>
2019-07-03 11:04:34 -0400

    Edit?

--- snmp.py
+++ snmp.py
@@ -1,36 +1,26 @@
 import asyncio
 
-class EchoClientProtocol:
-    def __init__(self, message, loop):
-        self.message = message
-        self.loop = loop
-        self.transport = None
-
+class EchoServerProtocol:
     def connection_made(self, transport):
         self.transport = transport
-        print('Send:', self.message)
-        self.transport.sendto(self.message.encode())
 
     def datagram_received(self, data, addr):
-        print("Received:", data.decode())
-
-        print("Close the socket")
-        self.transport.close()
-
-    def error_received(self, exc):
-        print('Error received:', exc)
-
-    def connection_lost(self, exc):
-        print("Socket closed, stop the event loop")
-        loop = asyncio.get_event_loop()
-        loop.stop()
+        message = data.decode()
+        print('Received %r from %s' % (message, addr))
+        print('Send %r to %s' % (message, addr))
+        self.transport.sendto(data, addr)
 
 loop = asyncio.get_event_loop()
-message = "Hello World!"
-connect = loop.create_datagram_endpoint(
-    lambda: EchoClientProtocol(message, loop),
-    remote_addr=('0.0.0.0', 161))
-transport, protocol = loop.run_until_complete(connect)
-loop.run_forever()
+print("Starting UDP server")
+# One protocol instance will be created to serve all client requests
+listen = loop.create_datagram_endpoint(
+    EchoServerProtocol, local_addr=('0.0.0.0', 161))
+transport, protocol = loop.run_until_complete(listen)
+
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+
 transport.close()
 loop.close()
