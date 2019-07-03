94a1d8c2dbf88774588a3bed6ace47a2e192063b
Tristan <tristanbienstman@gmail.com>
2019-07-03 13:41:52 -0400

    Edit?

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ from icarussyslog import syslogout
 from editor import editor
 from snmp import runsnmp
 from multiprocessing import Process, Lock
+import sys
 
 
 def get_ip_address():
@@ -76,7 +77,7 @@ def guiloop(window):
 
         key = w.getch()
         if key == ord('q'):
-            break
+            sys.exit()
         elif key == ord('p'):
             editor()  # from editor.py, opens your system editor.
             window.erase()
