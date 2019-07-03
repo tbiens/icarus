c0b3389fff71629dad38c03db51a2efc82756652
Tristan <tristanbienstman@gmail.com>
2019-07-03 09:34:04 -0400

    Edit?

---
+++ snmp.py
@@ -0,0 +1,15 @@
+import socketserver
+
+
+class snmpd(socketserver.BaseRequestHandler):
+    def handle(self):
+        data = self.request[0].strip()
+        socket = self.request[1]
+        print ("{} wrote:".format(self.client_address[0]))
+        print (data)
+        socket.sendto(data.upper(), self.client_address)
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+    with socketserver.UDPServer((HOST, PORT), MyUDPHandler) as server:
+        server.serve_forever()
