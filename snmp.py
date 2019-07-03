35e8b88eb11bc7c3dbb127c60f5fa7f1cc511d30
Tristan <tristanbienstman@gmail.com>
2019-07-03 10:46:45 -0400

    Edit?

--- snmp.py
+++ snmp.py
@@ -4,17 +4,6 @@ import threading
 
 ServerAddress = ("0.0.0.0", 161)
 
-
-class snmpd(socketserver.BaseRequestHandler):
-    def handle(self):
-        #  data = self.request[0].strip()
-        #  socket = self.request[1]
-        #  placeholders incase I want them back.
-        print(self.client_address[0])
-#      print (data)
-#        socket.sendto("bob", self.client_address)
-
-
 class Snmpd(socketserver.DatagramRequestHandler):
     def handle(self):
         print(self.client_address[0])
