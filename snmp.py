e73b347e6023af8a03ccddcfe673cffc7459795d
Tristan <tristanbienstman@gmail.com>
2019-07-03 11:03:26 -0400

    Edit?

--- snmp.py
+++ snmp.py
@@ -1,49 +1,36 @@
-import socketserver
 import asyncio
 
+class EchoClientProtocol:
+    def __init__(self, message, loop):
+        self.message = message
+        self.loop = loop
+        self.transport = None
 
-ServerAddress = ()
-
-class Snmpd(socketserver.DatagramRequestHandler):
-    def handle(self):
-        print(self.client_address[0])
-        #  future proof
-        #  self.wfile.write("Message from Server! Hello Client".encode())
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
-
-
-
-
-class EchoServerProtocol:
     def connection_made(self, transport):
         self.transport = transport
+        print('Send:', self.message)
+        self.transport.sendto(self.message.encode())
 
     def datagram_received(self, data, addr):
-        message = data.decode()
-        print('Received %r from %s' % (message, addr))
-        print('Send %r to %s' % (message, addr))
-        self.transport.sendto(data, addr)
+        print("Received:", data.decode())
 
+        print("Close the socket")
+        self.transport.close()
 
-async def main():
-    print("Starting UDP server")
+    def error_received(self, exc):
+        print('Error received:', exc)
 
-    # Get a reference to the event loop as we plan to use
-    # low-level APIs.
-    loop = asyncio.get_running_loop()
+    def connection_lost(self, exc):
+        print("Socket closed, stop the event loop")
+        loop = asyncio.get_event_loop()
+        loop.stop()
 
-    # One protocol instance will be created to serve all
-    # client requests.
-    transport, protocol = await loop.create_datagram_endpoint(
-        lambda: EchoServerProtocol(),
-        local_addr=('0.0.0.0', 161))
-
-    try:
-        await asyncio.sleep(3600)  # Serve for 1 hour.
-    finally:
-        transport.close()
-
-
+loop = asyncio.get_event_loop()
+message = "Hello World!"
+connect = loop.create_datagram_endpoint(
+    lambda: EchoClientProtocol(message, loop),
+    remote_addr=('0.0.0.0', 161))
+transport, protocol = loop.run_until_complete(connect)
+loop.run_forever()
+transport.close()
+loop.close()
