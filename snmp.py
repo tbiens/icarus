72c428512870ee46ca913ef76b936c36ff3f032f
Tristan <tristanbienstman@gmail.com>
2019-07-03 12:04:22 -0400

    Edit?

--- snmp.py
+++ snmp.py
@@ -1,14 +1,12 @@
 import asyncio
 from abuseipdb import snmpabuseipdb
 
+
 class icarus:
-    async def connection_made(self, transport):
-        await transport
+    def connection_made(self, transport):
         self.transport = transport
 
-    async def datagram_received(self, data, addr):
-        await data
-        await addr
+    def datagram_received(self, data, addr):
         #print(data)
         print(addr[0])
         #snmpabuseipdb(addr[0])
@@ -17,15 +15,19 @@ class icarus:
 #        print('Send %r to %s' % (message, addr))
 #        self.transport.sendto(data, addr)
 
+
 loop = asyncio.get_event_loop()
 
 listen = loop.create_datagram_endpoint(icarus, local_addr=('0.0.0.0', 161))
 transport, protocol = loop.run_until_complete(listen)
 
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
+loop.run_forever()
+
+
+#try:
+#
+#except KeyboardInterrupt:
+#    pass
 
 transport.close()
 loop.close()
