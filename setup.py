50ca86cad43480c9d0bc95bd4189aabcb718a65c
Tristan <tristanbienstman@gmail.com>
2019-09-18 12:18:36 -0400

    Edit?

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ def main(window):
     guiloop(window)
     # threading just wouldnt work. Process does seem to work.
     controller.stop()
-    p1.terminate()
+    p1.kill()
 
 
 class smtphoney:
