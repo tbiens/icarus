2783f9e01d12ac3cf50b9a45aefd10db887ccf21
Tristan <tristanbienstman@gmail.com>
2020-02-13 13:51:48 -0500

    SMB!

--- smb.py
+++ smb.py
@@ -3,46 +3,34 @@ import logging
 from abuseipdb import snmpabuseipdb
 
 
-class icarus:
+class EchoServerProtocol(asyncio.Protocol):
     def connection_made(self, transport):
+        peername = transport.get_extra_info('peername')
+        print('Connection from {}'.format(peername))
         self.transport = transport
 
-    def datagram_received(self, data, addr):
-        #print(data)
-        #print(addr[0])
-        snmpabuseipdb(addr[0])
-        #message = data.decode()
-        #print('Received %r from %s' % (message, addr))
-#        print('Send %r to %s' % (message, addr))
-#        self.transport.sendto(data, addr)
+    def data_received(self, data):
+        message = data.decode()
+        print('Data received: {!r}'.format(message))
+
+        print('Send: {!r}'.format(message))
+        self.transport.write(data)
+
+        print('Close the client socket')
+        self.transport.close()
 
 
-def runsnmp():
-    loop = asyncio.get_event_loop()
-    asyncio.set_event_loop(loop)
-    listen = loop.create_datagram_endpoint(icarus, local_addr=('0.0.0.0', 161))
-    transport, protocol = loop.run_until_complete(listen)
+async def main():
+    # Get a reference to the event loop as we plan to use
+    # low-level APIs.
+    loop = asyncio.get_running_loop()
 
-    loop.run_forever()
-    transport.close()
-    loop.close()
+    server = await loop.create_server(
+        lambda: EchoServerProtocol(),
+        '0.0.0.0', 445)
+
+    async with server:
+        await server.serve_forever()
 
 
-def runsmb(ip='0.0.0.0', port=445):
-    loop = asyncio.get_event_loop()
-    smb = asyncio.start_server(icarus, ip, port)
-    server = loop.run_until_complete(smb)
-    logging.info('Looking for connections on {}'.format(
-        server.sockets[0].getsockname()))
-
-    try:
-        logging.info("Press Control-C to stop the server.")
-        loop.run_until_complete(smb)
-    except KeyboardInterrupt:
-        logging.info("KeyboardInterrupt received: closing the server.")
-    finally:
-        server.close()
-        loop.run_until_complete(server.wait_closed())
-        loop.close()
-
-runsmb()
+asyncio.run(main())
