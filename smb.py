a635f361a9a0b3939de8a5ad742f1bedf290deec
Tristan <tristanbienstman@gmail.com>
2020-02-13 14:25:19 -0500

    SMB!

--- smb.py
+++ smb.py
@@ -3,23 +3,20 @@ from abuseipdb import hackingabuseipdb
 
 
 def runsmb():
-    s = socket.socket()
-    host = '0.0.0.0'
-    port = 445
-    s.bind((host, port))
-    s.listen()
-
-    conn, addr = s.accept()
-    # print(conn)
-    hackingabuseipdb(addr[0])
-
     while 1:
-        data = conn.recv(1024)
-        if not data: break
-        #print(data)
+        s = socket.socket()
+        host = '0.0.0.0'
+        port = 445
+        s.bind((host, port))
+        s.listen()
 
-    conn.close()
+        conn, addr = s.accept()
+        # print(conn)
+        hackingabuseipdb(addr[0])
 
+        while 1:
+            data = conn.recv(1024)
+            if not data: break
+            #print(data)
 
-while 1:
-    runsmb()
+        conn.close()
