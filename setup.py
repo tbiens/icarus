c486b91c4f6ee7985a7e7431631645b6eda96302
Tristan <tristanbienstman@gmail.com>
2020-02-14 12:30:06 -0500

    SMB!

--- setup.py
+++ setup.py
@@ -89,6 +89,7 @@ def main(window):
 
         w.addstr(0, 0, "Listening on: " + IP)
         w.addstr(1, 0, "Server started. Press Q to quit.", curses.color_pair(1))
+        w.addstr(2, 0, "P2 Status:" + str(p2.is_alive()))
         # It always shows IP address it's listening on and showing you can hit Q to quit.
 
         w.refresh()
