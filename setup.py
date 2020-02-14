5a1de34d8809a7f73f5557ceb734bc1829e53368
Tristan <tristanbienstman@gmail.com>
2020-02-14 13:07:41 -0500

    SMB!

--- setup.py
+++ setup.py
@@ -95,7 +95,9 @@ def main(window):
             w.refresh()
         elif key == ord('a'):
             import os
-            os.execl(sys.executable, ['python' + sys.argv])
+            print(sys.executable)
+            print(sys.argv)
+            os.execl(sys.executable, ['python'] + sys.argv)
         elif key == ord('p'):
             editor()  # from editor.py, opens your system editor.
             w.erase()
