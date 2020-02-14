90503bfa7a49718e3dc7e062591565c645b1afe5
Tristan <tristanbienstman@gmail.com>
2020-02-14 10:08:04 -0500

    SMB!

--- smb.py
+++ smb.py
@@ -8,6 +8,7 @@ def runsmb():
         s = socket.socket()
         host = '0.0.0.0'
         port = 445
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((host, port))
         s.listen()
 
@@ -21,4 +22,4 @@ def runsmb():
             #print(data)
 
         conn.close()
-        time.sleep(30)
+        time.sleep(1)
