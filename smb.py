96db848e807d63e9f4c1e0a9dbf2348b3353e70b
Tristan <tristanbienstman@gmail.com>
2020-02-18 12:21:43 -0500

    Reset screen bug

--- smb.py
+++ smb.py
@@ -1,11 +1,8 @@
 import socketserver
-import time
 from abuseipdb import hackingabuseipdb
 
 
-
 class MyTCPHandler(socketserver.BaseRequestHandler):
-
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
@@ -14,8 +11,6 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 
 def runsmb():
     HOST, PORT = "0.0.0.0", 445
-
     server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
     server.allow_reuse_address = True
-
     server.serve_forever()
