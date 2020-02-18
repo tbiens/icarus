430258a0f6c771c8e46917fa45bb857ab192f60f
Tristan <tristanbienstman@gmail.com>
2020-02-18 12:13:15 -0500

    Reset screen bug

--- smb.py
+++ smb.py
@@ -9,7 +9,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
-        hackingabuseipdb(self.client_address[0]))
+        hackingabuseipdb(self.client_address[0])
 
 
 def runsmb():
