6aae1456b8e405abea0985bf4c1055ab7f01a496
Tristan <tristanbienstman@gmail.com>
2020-02-18 12:12:23 -0500

    Reset screen bug

--- smb.py
+++ smb.py
@@ -3,22 +3,18 @@ import time
 from abuseipdb import hackingabuseipdb
 
 
+
 class MyTCPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+        hackingabuseipdb(self.client_address[0]))
 
-if __name__ == "__main__":
+
+def runsmb():
     HOST, PORT = "0.0.0.0", 445
 
-    # Create the server, binding to localhost on port 9999
     server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
 
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
     server.serve_forever()
