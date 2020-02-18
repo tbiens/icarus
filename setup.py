86c7deb1eb821dbbeff723b0e296cd327080476f
Tristan <tristanbienstman@gmail.com>
2020-02-18 13:00:48 -0500

    Reset screen bug

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ def main(window):
             w.refresh()
             # window.addstr(2,0,"You pressed P\n") # Just a place holder for new commands in the future.
 
-        sleep(3)  # So that the screen isn't refreshing at crazy rates unnecessarily.
+        w.napms(3000)  # So that the screen isn't refreshing at crazy rates unnecessarily.
 
     # threading just wouldnt work. Process does seem to work.
     controller.stop()
