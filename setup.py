a39d7854ac018634558eb3d50010e1efe5b484f3
Tristan <tristanbienstman@gmail.com>
2020-06-25 12:49:42 -0400

    working?

--- setup.py
+++ setup.py
@@ -103,9 +103,11 @@ def main(window):
         p9 = Process(name='SMTP', target=startsmtp(), daemon=True)
         p9.start()
 
+    print(enableTCP)
+    time.sleep(5)
     for port in enableTCP:
-        #print(port)
-        #time.sleep(5)
+        print(port)
+        time.sleep(5)
         tcpgen = Process(name='DynamicTCP ' + str(port), target=runtcp, daemon=True, args=(port,))
         tcpgen.start()
 
