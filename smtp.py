fdc585be8c9e32abb1cecebd3b0f7a6a97df5cd0
Tristan <tristanbienstman@gmail.com>
2020-06-26 08:44:10 -0400

    smtp running?

--- smtp.py
+++ smtp.py
@@ -51,3 +51,4 @@ def startsmtp():
     controller = Controller(smtphoney(), hostname=IP, port=smtpport)
     # It calls the class below as my handler, the hostname sets the ip, I set the SMTP port to 25 obviously
     controller.start()
+    input("Server Started, and is waiting for input before dying. You likely won't ever see this.")
