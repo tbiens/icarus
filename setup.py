98e70d3d1683bf9b8bf6739aa4be9e739abec540
Tristan <tristanbienstman@gmail.com>
2020-07-20 13:12:43 -0400

    logging

--- setup.py
+++ setup.py
@@ -139,8 +139,10 @@ def main(window):
         w.addstr(2, 0, "Dynamic Ports:")
 
         dynports = active_children()
-        for num, disport in enumerate(dynports, start=1):
-            w.addstr((num + 3), 0, "{}".format(disport))
+        for bob in active_children():
+            w.addstr(3, 0, bob.name)
+        #for num, disport.name in enumerate(dynports, start=1):
+        #    w.addstr((num + 3), 0, "{}".format(disport))
 
         w.addstr(13, 0, "Last 5 Attackers: ", curses.color_pair(3))
         attackerlist = getlastattackers()
