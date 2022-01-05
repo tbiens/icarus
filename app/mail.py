15a0c961f6abd1ede4f78a7b45b1c1e1a35aa1ae
RiversideRocks <wilest7@gmail.com>
2022-01-05 08:55:18 -0500

    Add ports to mail config

--- app/mail.py
+++ app/mail.py
@@ -9,7 +9,46 @@ import ipaddress
 from datetime import datetime
 import requests  # https://developers.virustotal.com/v2.0/reference#file-scan
 import app.cfg
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from ipwhois import IPWhois
+import sys
+import datetime
 
 config = configparser.ConfigParser()
 config.read('icarus.config')
 enabled = config['MAIL']['Mail']
+
+def sendEmail(ipaddr, preport):
+    obj = IPWhois(ipaddr)
+    emails = obj.lookup_whois()["nets"][0]["emails"]
+
+    try:
+        abuse = emails[0]
+    except:
+        print("No abuse contact")
+        sys.exit()
+    timestamp = datetime.datetime.now()
+    prenatport = preport.strip()
+    natports = {
+        "2021": "21",
+        "2022": "22",
+        "2023": "23",
+        "2205": "25",
+        "20110": "110",
+        "20111": "111",
+        "20135": "135",
+        "20139": "139",
+        "20143": "143",
+        "20161": "161",
+        "20445": "445",
+        "1433": "1433",
+        "1723": "1723",
+        "3306": "3306",
+        "3389": "3389",
+        "5600": "5600",
+        "5900": "5900"
+    }
+    port = natports[prenatport]
+    
